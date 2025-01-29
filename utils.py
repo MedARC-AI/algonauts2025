fa1552b9230f605532e972fc14d8f4ac8c875e24
Mihir Tripathy <hello@mihirneal.com>
2025-01-29 14:24:06 +0000

    data preproc + alignment fixes

---
+++ utils.py
@@ -0,0 +1,368 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import os
+import h5py
+
+def preprocess_features(features):
+    """
+    Rplaces NaN values in the stimulus features with zeros, and z-score the
+    features.
+
+    Parameters
+    ----------
+    features : float
+        Stimulus features.
+
+    Returns
+    -------
+    prepr_features : float
+        Preprocessed stimulus features.
+
+    """
+
+    ### Convert NaN values to zeros ###
+    features = np.nan_to_num(features)
+
+    ### Z-score the features ###
+    scaler = StandardScaler()
+    prepr_features = scaler.fit_transform(features)
+
+    ### Output ###
+    return prepr_features
+
+
+def perform_pca(prepr_features, n_components):
+    """
+    Perform PCA on the standardized features.
+
+    Parameters
+    ----------
+    prepr_features : float
+        Preprocessed stimulus features.
+    n_components : int
+        Number of components to keep
+
+    Returns
+    -------
+    features_pca : float
+        PCA-downsampled stimulus features.
+
+    """
+
+    ### Set the number of principal components to keep ###
+    # If number of PCs is larger than the number of features, set the PC number
+    # to the number of features
+    if n_components > prepr_features.shape[1]:
+        n_components = prepr_features.shape[1]
+
+    ### Perform PCA ###n_init=4, max_iter=300
+    pca = PCA(n_components, random_state=42)
+    features_pca = pca.fit_transform(prepr_features)
+
+    ### Output ###
+    return features_pca
+
+def count_total_files(features_dir, movie_list):
+    total_files = 0
+    for movie in movie_list:
+        if 'friends' in movie:
+            season = movie.split('-')[1]
+            pattern = f"{season}e"
+        else:
+            movie_name = movie.replace('movie10-', '')
+            pattern = movie_name
+            
+        files = [f for f in os.listdir(features_dir + 'audio') 
+                if '_features_' in f and pattern in f]
+        total_files += len(files)
+            
+    return total_files
+
+def get_fmri_files(subject, task_type, fmri_dir):
+    subject_dir = os.path.join(fmri_dir, f'sub-{subject}/func/')
+    files = [f for f in os.listdir(subject_dir) if f'sub-{subject}_task-{task_type}' in f]
+    if len(files) != 1:
+        raise ValueError(f"Expected 1 fMRI file for subject {subject} and task {task_type}, found {len(files)}")
+    return os.path.join(subject_dir, files[0])
+
+def load_fmri(root_data_dir, subject):
+    fmri = {}
+
+    fmri_file = f'sub-0{subject}_task-friends_space-MNI152NLin2009cAsym_atlas-Schaefer18_parcel-1000Par7Net_desc-s123456_bold.h5'
+    fmri_dir = os.path.join(root_data_dir, f'sub-0{subject}', 'func', fmri_file)
+    fmri_friends = h5py.File(fmri_dir, 'r')
+    for key, val in fmri_friends.items():
+        fmri[str(key[13:])] = val[:].astype(np.float32)
+    del fmri_friends
+
+    fmri_file = f'sub-0{subject}_task-movie10_space-MNI152NLin2009cAsym_atlas-Schaefer18_parcel-1000Par7Net_bold.h5'
+    fmri_dir = os.path.join(root_data_dir, f'sub-0{subject}', 'func', fmri_file)
+    fmri_movie10 = h5py.File(fmri_dir, 'r')
+    for key, val in fmri_movie10.items():
+        fmri[key[13:]] = val[:].astype(np.float32)
+    del fmri_movie10
+
+    keys_all = fmri.keys()
+    figures_splits = 12
+    for s in range(figures_splits):
+        movie = 'figures' + format(s+1, '02')
+        keys_movie = [rep for rep in keys_all if movie in rep]
+        fmri[movie] = ((fmri[keys_movie[0]] + fmri[keys_movie[1]]) / 2).astype(np.float32)
+        del fmri[keys_movie[0]]
+        del fmri[keys_movie[1]]
+
+    keys_all = fmri.keys()
+    life_splits = 5
+    for s in range(life_splits):
+        movie = 'life' + format(s+1, '02')
+        keys_movie = [rep for rep in keys_all if movie in rep]
+        fmri[movie] = ((fmri[keys_movie[0]] + fmri[keys_movie[1]]) / 2).astype(np.float32)
+        del fmri[keys_movie[0]]
+        del fmri[keys_movie[1]]
+
+    return fmri
+
+def train_baseline_encoding(features_train, fmri_train):
+    """
+    Train a linear-regression-based encoding model to predict fMRI responses
+    using movie features.
+
+    Parameters
+    ----------
+    features_train : float
+        Stimulus features for the training movies.
+    fmri_train : float
+        fMRI responses for the training movies.
+
+    Returns
+    -------
+    model : object
+        Trained regression model.
+
+    """
+
+    ### Train the linear regression model ###
+    model = LinearRegression().fit(features_train, fmri_train)
+
+    ### Output ###
+    return model
+
+def load_fmri(root_data_dir, subject):
+    """
+    Load the fMRI responses for the selected subject.
+
+    Parameters
+    ----------
+    root_data_dir : str
+        Root data directory.
+    subject : int
+        Subject used to train and validate the encoding model.
+
+    Returns
+    -------
+    fmri : dict
+        Dictionary containing the  fMRI responses.
+
+    """
+
+    fmri = {}
+
+    ### Load the fMRI responses for Friends ###
+    # Data directory
+    fmri_file = f'sub-0{subject}_task-friends_space-MNI152NLin2009cAsym_atlas-Schaefer18_parcel-1000Par7Net_desc-s123456_bold.h5'
+    fmri_dir = os.path.join(root_data_dir, f'sub-0{subject}', 'func', fmri_file)
+    # Load the the fMRI responses
+    fmri_friends = h5py.File(fmri_dir, 'r')
+    for key, val in fmri_friends.items():
+        fmri[str(key[13:])] = val[:].astype(np.float32)
+    del fmri_friends
+
+    ### Load the fMRI responses for Movie10 ###
+    # Data directory
+    fmri_file = f'sub-0{subject}_task-movie10_space-MNI152NLin2009cAsym_atlas-Schaefer18_parcel-1000Par7Net_bold.h5'
+    fmri_dir = os.path.join(root_data_dir, f'sub-0{subject}', 'func', fmri_file)
+    # Load the the fMRI responses
+    fmri_movie10 = h5py.File(fmri_dir, 'r')
+    for key, val in fmri_movie10.items():
+        fmri[key[13:]] = val[:].astype(np.float32)
+    del fmri_movie10
+    # Average the fMRI responses across the two repeats for 'figures'
+    keys_all = fmri.keys()
+    figures_splits = 12
+    for s in range(figures_splits):
+        movie = 'figures' + format(s+1, '02')
+        keys_movie = [rep for rep in keys_all if movie in rep]
+        fmri[movie] = ((fmri[keys_movie[0]] + fmri[keys_movie[1]]) / 2).astype(np.float32)
+        del fmri[keys_movie[0]]
+        del fmri[keys_movie[1]]
+    # Average the fMRI responses across the two repeats for 'life'
+    keys_all = fmri.keys()
+    life_splits = 5
+    for s in range(life_splits):
+        movie = 'life' + format(s+1, '02')
+        keys_movie = [rep for rep in keys_all if movie in rep]
+        fmri[movie] = ((fmri[keys_movie[0]] + fmri[keys_movie[1]]) / 2).astype(np.float32)
+        del fmri[keys_movie[0]]
+        del fmri[keys_movie[1]]
+
+    ### Output ###
+    return fmri
+
+def align_features_and_fmri_samples(features, fmri, excluded_samples_start,
+    excluded_samples_end, hrf_delay, stimulus_window, movies):
+    """
+    Align the stimulus feature with the fMRI response samples for the selected
+    movies, later used to train and validate the encoding models.
+
+    Parameters
+    ----------
+    features : dict
+        Dictionary containing the stimulus features.
+    fmri : dict
+        Dictionary containing the fMRI responses.
+    excluded_trs_start : int
+        Integer indicating the first N fMRI TRs that will be excluded and not
+        used for model training. The reason for excluding these TRs is that due
+        to the latency of the hemodynamic response the fMRI responses of first
+        few fMRI TRs do not yet contain stimulus-related information.
+    excluded_trs_end : int
+        Integer indicating the last N fMRI TRs that will be excluded and not
+        used for model training. The reason for excluding these TRs is that
+        stimulus feature samples (i.e., the stimulus chunks) can be shorter than
+        the fMRI samples (i.e., the fMRI TRs), since in some cases the fMRI run
+        ran longer than the actual movie. However, keep in mind that the fMRI
+        timeseries onset is ALWAYS SYNCHRONIZED with movie onset (i.e., the
+        first fMRI TR is always synchronized with the first stimulus chunk).
+    hrf_delay : int
+        fMRI detects the BOLD (Blood Oxygen Level Dependent) response, a signal
+        that reflects changes in blood oxygenation levels in response to
+        activity in the brain. Blood flow increases to a given brain region in
+        response to its activity. This vascular response, which follows the
+        hemodynamic response function (HRF), takes time. Typically, the HRF
+        peaks around 5–6 seconds after a neural event: this delay reflects the
+        time needed for blood oxygenation changes to propagate and for the fMRI
+        signal to capture them. Therefore, this parameter introduces a delay
+        between stimulus chunks and fMRI samples for a better correspondence
+        between input stimuli and the brain response. For example, with a
+        hrf_delay of 3, if the stimulus chunk of interest is 17, the
+        corresponding fMRI sample will be 20.
+    stimulus_window : int
+        Integer indicating how many stimulus features' chunks are used to model
+        each fMRI TR, starting from the chunk corresponding to the TR of
+        interest, and going back in time. For example, with a stimulus_window of
+        5, if the fMRI TR of interest is 20, it will be modeled with stimulus
+        chunks [16, 17, 18, 19, 20]. Note that this only applies to visual and
+        audio features, since the language features were already extracted using
+        transcript words spanning several movie chunks (thus, each fMRI TR will
+        only be modeled using the corresponding language feature chunk). Also
+        note that a larger stimulus window will increase compute time, since it
+        increases the amount of stimulus features used to train and test the
+        fMRI encoding models.
+    movies: list
+        List of strings indicating the movies for which the fMRI responses and
+        stimulus features are aligned, out of the first six seasons of Friends
+        ["friends-s01", "friends-s02", "friends-s03", "friends-s04",
+        "friends-s05", "friends-s06"], and the four movies from Movie10
+        ["movie10-bourne", "movie10-figures", "movie10-life", "movie10-wolf"].
+
+    Returns
+    -------
+    aligned_features : float
+        Aligned stimulus features for the selected movies.
+    aligned_fmri : float
+        Aligned fMRI responses for the selected movies.
+
+    """
+
+    ### Empty data variables ###
+    aligned_features = []
+    aligned_fmri = np.empty((0,1000), dtype=np.float32)
+
+    ### Loop across movies ###
+    for movie in movies:
+
+        ### Get the IDs of all movies splits for the selected movie ###
+        if movie[:7] == 'friends':
+            id = movie[8:]
+        elif movie[:7] == 'movie10':
+            id = movie[8:]
+        movie_splits = [key for key in fmri if id in key[:len(id)]]
+
+        ### Loop over movie splits ###
+        for split in movie_splits:
+
+            ### Extract the fMRI ###
+            fmri_split = fmri[split]
+            # Exclude the first and last fMRI samples
+            fmri_split = fmri_split[excluded_samples_start:-excluded_samples_end]
+            aligned_fmri = np.append(aligned_fmri, fmri_split, 0)
+
+            ### Loop over fMRI samples ###
+            for s in range(len(fmri_split)):
+                # Empty variable containing the stimulus features of all
+                # modalities for each fMRI sample
+                f_all = np.empty(0)
+
+                ### Loop across modalities ###
+                for mod in features.keys():
+
+                    ### Visual and audio features ###
+                    # If visual or audio modality, model each fMRI sample using
+                    # the N stimulus feature samples up to the fMRI sample of
+                    # interest minus the hrf_delay (where N is defined by the
+                    # 'stimulus_window' variable)
+                    if mod == 'visual' or mod == 'audio':
+                        # In case there are not N stimulus feature samples up to
+                        # the fMRI sample of interest minus the hrf_delay (where
+                        # N is defined by the 'stimulus_window' variable), model
+                        # the fMRI sample using the first N stimulus feature
+                        # samples
+                        if s < (stimulus_window + hrf_delay):
+                            idx_start = excluded_samples_start
+                            idx_end = idx_start + stimulus_window
+                        else:
+                            idx_start = s + excluded_samples_start - hrf_delay \
+                                - stimulus_window + 1
+                            idx_end = idx_start + stimulus_window
+                        # In case there are less visual/audio feature samples
+                        # than fMRI samples minus the hrf_delay, use the last N
+                        # visual/audio feature samples available (where N is
+                        # defined by the 'stimulus_window' variable)
+                        if idx_end > (len(features[mod][split])):
+                            idx_end = len(features[mod][split])
+                            idx_start = idx_end - stimulus_window
+                        f = features[mod][split][idx_start:idx_end]
+                        f_all = np.append(f_all, f.flatten())
+
+                    ### Language features ###
+                    # Since language features already consist of embeddings
+                    # spanning several samples, only model each fMRI sample
+                    # using the corresponding stimulus feature sample minus the
+                    # hrf_delay
+                    elif mod == 'language':
+                        # In case there are no language features for the fMRI
+                        # sample of interest minus the hrf_delay, model the fMRI
+                        # sample using the first language feature sample
+                        if s < hrf_delay:
+                            idx = excluded_samples_start
+                        else:
+                            idx = s + excluded_samples_start - hrf_delay
+                        # In case there are fewer language feature samples than
+                        # fMRI samples minus the hrf_delay, use the last
+                        # language feature sample available
+                        if idx >= (len(features[mod][split]) - hrf_delay):
+                            f = features[mod][split][-1,:]
+                        else:
+                            f = features[mod][split][idx]
+                        f_all = np.append(f_all, f.flatten())
+
+                 ### Append the stimulus features of all modalities for this sample ###
+                aligned_features.append(f_all)
+
+    ### Convert the aligned features to a numpy array ###
+    aligned_features = np.asarray(aligned_features, dtype=np.float32)
+
+    ### Output ###
+    return aligned_features, aligned_fmri
