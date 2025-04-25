72621ac5484a12a9a1793c8072563f3b333c3c65
Jiaxin (Cindy) Tu <30844460+cindyhfls@users.noreply.github.com>
2025-04-25 15:32:23 -0500

    Update feature_extraction_utils.py

--- feature_extraction_utils.py
+++ feature_extraction_utils.py
@@ -3,7 +3,92 @@ import torchvision
 from typing import Optional, Callable, Tuple, Any
 from torchvision.datasets.vision import VisionDataset
 import cv2
+def extract_visual_features(episode_path, tr, feature_extractor, model_layer,
+    transform, device, save_dir_temp, save_dir_features):
+    """
+    Extract visual features from a movie using a pre-trained video model.
 
+    Parameters
+    ----------
+    episode_path : str
+        Path to the movie file for which the visual features are extracted.
+    tr : float
+        Duration of each chunk, in seconds (aligned with the fMRI repetition
+        time, or TR).
+    feature_extractor : torch.nn.Module
+        Pre-trained feature extractor model.
+    model_layer : str
+        The model layer from which the visual features are extracted.
+    transform : torchvision.transforms.Compose
+        Transformation pipeline for processing video frames.
+    device : torch.device
+        Device for computation ('cpu' or 'cuda').
+    save_dir_temp : str
+        Directory where the chunked movie clips are temporarily stored for
+        feature extraction.
+    save_dir_features : str
+        Directory where the extracted visual features are saved.
+
+    Returns
+    -------
+    visual_features : float
+        Array containing the extracted visual features.
+
+    """
+
+    # Get the onset time of each movie chunk
+    clip = VideoFileClip(episode_path)
+    start_times = [x for x in np.arange(0, clip.duration, tr)][:-1]
+    # Create the directory where the movie chunks are temporarily saved
+    temp_dir = os.path.join(save_dir_temp, 'temp')
+    os.makedirs(temp_dir, exist_ok=True)
+
+    # Empty features list
+    visual_features = []
+
+    # Loop over chunks
+    with tqdm(total=len(start_times), desc="Extracting visual features") as pbar:
+        for start in start_times:
+
+            # Divide the movie in chunks of length TR, and save the resulting
+            # clips as '.mp4' files
+            clip_chunk = clip.subclip(start, start+tr)
+            chunk_path = os.path.join(temp_dir, 'visual_chunk.mp4')
+            clip_chunk.write_videofile(chunk_path, verbose=False, audio=False,
+                logger=None)
+            # Load the frames from the chunked movie clip
+            video_clip = VideoFileClip(chunk_path)
+            chunk_frames = [frame for frame in video_clip.iter_frames()]
+
+            # Format the frames to shape:
+            # (batch_size, channels, num_frames, height, width)
+            frames_array = np.transpose(np.array(chunk_frames), (3, 0, 1, 2))
+            # Convert the video frames to tensor
+            inputs = torch.from_numpy(frames_array).float()
+            # Preprocess the video frames
+            inputs = transform(inputs).unsqueeze(0).to(device)
+
+            # Extract the visual features
+            with torch.no_grad():
+                preds = feature_extractor(inputs)
+            visual_features.append(np.reshape(preds[model_layer].cpu().numpy(), -1))
+
+            # Update the progress bar
+            pbar.update(1)
+
+    # Convert the visual features to float32
+    visual_features = np.array(visual_features, dtype='float32')
+
+    # Save the visual features
+    #out_file_visual = os.path.join(
+    #    save_dir_features, f'friends_s01e01a_features_visual.h5')
+    #with h5py.File(out_file_visual, 'a' if Path(out_file_visual).exists() else 'w') as f:
+    #    group = f.create_group("s01e01a")
+    #    group.create_dataset('visual', data=visual_features, dtype=np.float32)
+    #print(f"Visual features saved to {out_file_visual}")
+
+    # Output
+    return visual_features
 def load_mkv_file(movie_path):
     """
     Load video and audio data from the given .mkv movie file, and additionally
@@ -245,7 +330,7 @@ def interface_display_transcript_and_movie(movie_path, transcript_path):
 
     dropdown.observe(on_chunk_select, names='value')
 
-# Dataloader writen by AI (Use with Caution)
+# == Dataloader writen by AI (Use with Caution)
 class VideoDataset(VisionDataset):
     """
     Args:
