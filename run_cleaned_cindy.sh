789cb38fbdd1e2a7a9924ae619f54fd81d0b380b
Jiaxin (Cindy) Tu <30844460+cindyhfls@users.noreply.github.com>
2025-07-06 17:14:26 -0400

    Add files via upload

---
+++ run_cleaned_cindy.sh
@@ -0,0 +1,33 @@
+source /home/connor/algonauts2025.clean/.venv/bin/activate # This
+
+export OMP_NUM_THREADS=8
+export CUDA_VISIBLE_DEVICES=7
+
+now=$(TZ=America/New_York date '+%y%m%d%H%M')
+uv=/home/connor/.local/bin/uv
+
+# Model 1
+"$uv" run python train_feature_encoder.py \
+    --overrides \
+    out_dir=output/feature_encoding_enc_kernel-45_${now} \
+    model.encoder_kernel_size=45\
+    datasets_root=/home/connor/algonauts2025.clean/datasets/ #/dev/shm/algonauts2025
+
+"$uv" run python submit_feature_encoder.py \
+    --overrides \
+    out_dir=output/feature_encoding_enc_kernel-45_${now} \
+    datasets_root=/home/connor/algonauts2025.clean/datasets/ #/dev/shm/algonauts2025
+ 
+# Model 2
+# Model 3
+# ...
+
+# Ensemble prediction (can also be run using existing model predictions at any time)
+foldernames= None #"feature_encoding_enc_kernel-45_2507061610,feature_encoding_enc_kernel-42_2507061637"
+python3 ensemble_predictions.py --target_filename "fmri_predictions_friends_s7.npy" --foldernames $foldernames
+
+ #Issues: 
+ # 1. Connor set the data path to /dev/shm/algonauts2025 which I could not find, because of the way how llama features were coded differently than others, I have to point to Connor's home directory for those data features instead of the /data/algonauts_2025.features, can we make a symbolic link? 
+ # 2. I edited my version of ./config/default_feature_encoding.yaml so that we have more features available but still some are not available in Connor's home directory
+ # 2. The submit_feature_encoder.py model construction part still broke if other features are added even though it is supposed to load the config file and create the same model
+ # 3. Currently the best one is 'feature_encoding_enc_kernel-45_2507061610' but it looks like Connor tried encoding kernel with 49 size and it's worse than 33
