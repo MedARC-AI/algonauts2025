1871005af26a9fe16be6df820687d41362788417
Connor Lane <connor.lane858@gmail.com>
2025-07-26 01:34:18 +0000

    update scripts

--- scripts/run_ablations.sh
+++ scripts/run_ablations.sh
@@ -126,3 +126,39 @@ for ii in {0..4}; do
         "subjects=[${subs}]" \
         "out_dir=${out_dir}"
 done
+
+kernel_names=(
+    default
+    causal
+    positive
+    blockwise
+)
+causal_kernel=(
+    false
+    true
+    false
+    false
+)
+positive_kernel=(
+    false
+    false
+    true
+    false
+)
+blockwise_kernel=(
+    false
+    false
+    false
+    true
+)
+for ii in 0 1 2 3; do
+    name=${kernel_names[ii]}
+    echo "ablate kernel type: ${name}"
+    out_dir="${out_prefix}/kernel_type/${name}"
+    uv run python train_feature_encoder.py \
+        --overrides \
+        "model.encoder_causal=${causal_kernel[ii]}" \
+        "model.encoder_positive=${positive_kernel[ii]}" \
+        "model.encoder_blockwise=${blockwise_kernel[ii]}" \
+        "out_dir=${out_dir}"
+done
